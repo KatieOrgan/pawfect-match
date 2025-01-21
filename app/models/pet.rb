2c2cf5563722c4cb056745844b936e35d3b5d1cd
Hollie Jaques <holliejaques@gmail.com>
2025-01-21 23:29:13 +0700

    completed models

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,10 @@
 class Pet < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+
+  validates :pet_name, presence: true
+  validates :breed, presence: true
+  validates :age, presence: true
+  validates :size, presence: true
+  validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
+  has_many :pets, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_pic_url, presence: true, uniqueness: true
+  validates :bio, presence: true
+  validates :is_owner, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
