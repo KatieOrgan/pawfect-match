98e680cb62432ae0396f9025cb420c91abd2c32b
Joshua Thomas <jcthomas092@gmail.com>
2025-01-22 12:05:51 +0000

    Added views

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :pets
-  resources :bookings
+  resources :pets, only: [:index, :show]
+  resources :bookings, only: [:index, :new, :create, :edit, :update, :destroy]
   resources :users, except: [:index]
 end
