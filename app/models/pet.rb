83e28a918c7ec1f776f749d13389b74a92ea6b5e
Hollie Jaques <holliejaques@gmail.com>
2025-01-23 23:30:48 +0700

    added search function

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,11 @@
 class Pet < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_by_breed_and_size_and_description_and_pet_name_and_available,
+    against: [ :breed, :size, :description, :pet_name, :available],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
