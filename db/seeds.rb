87255b21ffd6970d831e62b6fd2803fd5a591176
Joshua Thomas <jcthomas092@gmail.com>
2025-01-23 18:51:55 +0000

    minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -92,12 +92,6 @@ puts "User created with an attached profile picture!"
 
 puts "Creating some pets..."
 
-# -------------------------------
-# Now let's seed some pets
-# -------------------------------
-
-puts "Creating some pets..."
-
 ziggy = Pet.create!(
   pet_name: 'Ziggy',
   breed: 'Springer Spaniel',
